e024420633e2bf25a0af306e584afb1804d180cb
devkrutikaroyals <kruteeka.royalswebtech@gmail.com>
2025-11-03 11:27:45 +0530

    kjsdbf

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,4 +1,3 @@
-// routes/transactionRoutes.js
 import express from "express";
 import {
   getTransactions,
@@ -8,24 +7,27 @@ import {
   handlePaymentWebhook,
   getTransactionDetails,
   testConnection,
-  
-  testDatabaseConnection,testSchemaValidation
-
+  debugDefaultQRSimple,
+  testDatabaseConnection,
+  testSchemaValidation
 } from "../controllers/transactionController.js";
 import { authenticateMerchant } from "../middleware/authMiddleware.js";
-import  { validateTransactionData } from "../middleware/validationMiddleware.js";
-const router = express.Router();
-// All routes protected with merchant authentication
+import { validateTransactionData } from "../middleware/validationMiddleware.js";
 
-// In your routes file, add:
+const router = express.Router();
+
+// Debug routes
+router.get('/debug-simple', authenticateMerchant, debugDefaultQRSimple);
 router.get('/test-schema', authenticateMerchant, testSchemaValidation);
 router.get('/test-db', authenticateMerchant, testDatabaseConnection);
+
+// Main routes
 router.get("/", authenticateMerchant, getTransactions);
 router.post("/generate-qr", authenticateMerchant, validateTransactionData, generateDynamicQR);
-router.post("/default-qr", authenticateMerchant,  generateDefaultQR);
+router.post("/default-qr", authenticateMerchant, generateDefaultQR);
 router.get("/status/:transactionId", authenticateMerchant, checkTransactionStatus);
 router.get("/details/:transactionId", authenticateMerchant, getTransactionDetails);
-router.get("/test", authenticateMerchant, testConnection); // Add test endpoint
+router.get("/test", authenticateMerchant, testConnection);
 
 // Webhook doesn't need authentication
 router.post("/webhook", handlePaymentWebhook);
